49fca13ba752e31a650eeb94d84c4a9e7634a644
bambolelooo <63548697+bambolelooo@users.noreply.github.com>
2022-11-18 23:42:01 -0500

    started working on fetching excercises

--- assets/js/landing.js
+++ assets/js/landing.js
@@ -1,10 +1,12 @@
 // sample user profile according to our list
 const userData = {
     gender: 'male',
+    // unsure if age is needed because very hard to set excercise list according to age and body type
     age: '35',
     bodyType: 'endomorph', // ectomorph, endomorph, mesomorph (to avoid rude namings)
     weight: '70', // in kg
     height: '175', // in cm
-    goal: 'gain', // lose, gain, mantain
+    goal: 'gain', // lose, gain
+    place: 'home', // home or gym for picking equipment for excercises
 }
 localStorage.setItem(userData, JSON.stringify(userData))
