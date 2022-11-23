4747555fba6c24a6bdb2aaa37c8b33c7b90b375d
bambolelooo <63548697+bambolelooo@users.noreply.github.com>
2022-11-22 19:15:03 -0500

    added weekdays div

--- assets/js/services/mealplans_api.js
+++ assets/js/services/mealplans_api.js
@@ -1,3 +1,19 @@
 // https://developer.edamam.com/edamam-docs-recipe-api
+// sample request url: https://api.edamam.com/api/recipes/v2?type=public&app_id=df0d49c4&app_key=9571bca2f3520f672c7afcdda913fbf7&diet=high-protein&health=fish-free&health=gluten-free&health=low-sugar
 const EDAMAM_API_APP_ID = "df0d49c4";
 const EDAMAM_API_APP_KEY = "9571bca2f3520f672c7afcdda913fbf7";
+
+//for now I will create a sample mealplanUserData, later it should be recieved frow landing
+
+// Callories division is: 18% for breakfast, 30% for lunch, 30% for dinner, 11% for snack 1, 11% for snack 2.
+// Range for each meal would be 10% of the callories per meal. For example we have 2000 cals per day, so its 600 cals for lunch and the range would be 540-660 cals for lunch
+
+const mealplanUserData = {
+    calloriesPerDay: "2000",
+    preferences: ["pork-free", "peanut-free", "fish-free"],
+};
+
+// mealplan will be updated daily
+const date = new Date();
+const currentDateString =
+    date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();
