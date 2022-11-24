9fb39d288de1777ae3a6e7bffb8670ea0dea0a48
Lavanya Virushan <lavanya.virushan@gmail.com>
2022-11-24 18:29:18 -0500

    Fixed URL links

--- assets/js/dispatcher/workout_video.js
+++ assets/js/dispatcher/workout_video.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
-    const selected_Video = getSelectedWorkout();
+    const selected_Video = getSelectedWorkout(); //Reads the data from localstoarge
     /**
      * After DOM is ready we call the youtube api to get the video
      * Localstorage to get the currently selected workout
      * Call the UI to update the view
      */
-    getWorkoutVideoAPI(`${selected_Video.name}${selected_Video.type}`, function(video){
+    getWorkoutVideoAPI(`${selected_Video.name}${selected_Video.type}`, function(video){ // video is the result from youtube and it is an array
         renderWorkoutVideoUI(selected_Video, video[0].id.videoId)
     })
 });
